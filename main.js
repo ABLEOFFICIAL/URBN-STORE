ae096183c4c547354cd51c81eb1c0f3810fe38e0
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2025-02-04 21:29:09 +0100

    wip

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // newsleteter slide
 let info1 = document.getElementById('info-1');
 let info2 = document.getElementById('info-2');
+// sidebar
+let sideBar = document.getElementById('side-bar');
+let sideBarBtn = document.querySelector('#side-bar-btn');
+let xBtn = document.getElementById('x-btn');
+// product listing category
+let products = document.getElementById('products');
+let productCategory = document.getElementById('product-category');
 
 // newsletter animation
 window.addEventListener('load', () => {
@@ -24,3 +31,28 @@ slidenewsOut = () => {
     }, 3000)
 
 }
+
+// show sidebar
+function showSideBar(){
+    sideBar.classList.remove('hidden');
+}
+sideBarBtn.addEventListener('click', showSideBar);
+
+// remove side bar
+function removeSideBar(event){
+    event.stopPropagation();
+        sideBar.classList.add('hidden');
+}
+xBtn.addEventListener('click', removeSideBar);
+document.addEventListener('click', (event) => {
+   if(!sideBarBtn.contains(event.target) && !sideBar.contains(event.target)){
+    sideBar.classList.add('hidden');
+   }
+});
+
+// select product category
+products.addEventListener('click', () => {
+    productCategory.classList.toggle('hidden');
+    // productCategory.classList.add('flex');
+})
+
