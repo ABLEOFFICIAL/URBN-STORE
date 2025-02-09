ef82d1b1060f860a8e02c27258bee1adca8c7694
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2025-02-09 21:57:15 +0100

    error message

--- createAcct/createAcct.js
+++ createAcct/createAcct.js
@@ -8,30 +8,24 @@ let confirmPassword = document.getElementById('confirm-password');
 form.addEventListener('submit', (e) => {
 
     let inputs = [fullName, email, password, confirmPassword];
+    const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // error function
+    function error(label, message){
+        let invalid = document.createElement('p');
+        invalid.classList.add('text-red-500', 'absolute', 'top-1', 'right-3', 'border-2', 'border-red-500', 'rounded-full', 'px-2.5', 'py-1', 'text-xs', 'text-center', 'invalid-message');
+        invalid.textContent = message;
+        label.parentElement.appendChild(invalid);
+        setTimeout(() => {
+            invalid.classList.add('hidden');
+        }, 2000)
+
+    }
+
     inputs.forEach(input => {
         if(input.value === ''){
             e.preventDefault();
-            function error(){
-                let alertError = document.createElement('p');
-                alertError.textContent = 'i'
-                // alertError.className = 'fa-solid fa-circle-exclamation';
-                alertError.classList.add('text-red-500', 'absolute', 'top-1', 'right-3', 'border-2', 'border-red-500', 'rounded-full', 'px-2.5', 'py-1', 'text-xs', 'text-center');
-                input.parentElement.appendChild(alertError);
-                setTimeout(() => {
-                    alertError.remove();
-                }, 2000)
-            };
-
-            error();
+            error(input, 'i');
         }
+        })
     })
-    // if((fullName.value === '') || (email.value === '') || (password.value === '') || (confirmPassword.value === '')){
-    //     e.preventDefault();
-    //     let alert = document.createElement('i');
-    //     alert.className = 'fa-solid fa-circle-exclamation';
-    //     alert.classList.add('text-red-500');
-    //     console.log(alert);
-        
-        
-    })
-// })
